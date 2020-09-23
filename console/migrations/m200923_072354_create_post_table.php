492814a7e93434a645d0bfea4b0858ee2317d83e
antonch <antonchromjak@gmail.com>
2020-09-23 09:42:14 +0200

    timestamp

---
+++ console/migrations/m200923_072354_create_post_table.php
@@ -0,0 +1,64 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%post}}`.
+ * Has foreign keys to the tables:
+ *
+ * - `{{%user}}`
+ */
+class m200923_072354_create_post_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%post}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(512)->notNull(),
+            'perex' => $this->text()(),
+            'content' => $this->text()(),
+            'publishedAt' => $this->timestamp()()->notNull(),
+            'userId' => $this->integer(11),
+        ]);
+
+        // creates index for column `userId`
+        $this->createIndex(
+            '{{%idx-post-userId}}',
+            '{{%post}}',
+            'userId'
+        );
+
+        // add foreign key for table `{{%user}}`
+        $this->addForeignKey(
+            '{{%fk-post-userId}}',
+            '{{%post}}',
+            'userId',
+            '{{%user}}',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        // drops foreign key for table `{{%user}}`
+        $this->dropForeignKey(
+            '{{%fk-post-userId}}',
+            '{{%post}}'
+        );
+
+        // drops index for column `userId`
+        $this->dropIndex(
+            '{{%idx-post-userId}}',
+            '{{%post}}'
+        );
+
+        $this->dropTable('{{%post}}');
+    }
+}
